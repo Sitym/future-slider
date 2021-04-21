1314629f27ee1264e48f21cab81af619f143e1a6
mahady-manana <rm.mahady@gmail.com>
2021-04-21 19:32:44 +0300

    first commit

---
+++ packages/boom/ts.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+exports.test = 5;
+//# sourceMappingURL=ts.js.map
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './baam/tsxx'
+export * from './boom/ts'
